3daf1dc510541404dfcb260cd7092733a6d68866
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-13 13:57:16 +0900

    [refactor] raise TypeError Exceptions

--- baseballGame/test_game.py
+++ baseballGame/test_game.py
@@ -8,10 +8,15 @@ class TestGame(TestCase):
         super().setUp()
         self.game = Game()
 
-    def test_exceptipn_when_type_is_None(self):
-        with self.assertRaises(TypeError):
-            self.game.guess(None)
+    def assert_illegal_argument(self, guess_num):
+        try:
+            self.game.guess(guess_num)
+            self.fail()
+        except TypeError:
+            pass
 
-    def test_exception_when_input_len_is_not_3(self):
-        with self.assertRaises(TypeError):
-            self.game.guess("12")
+    def test_exception_when_invalid_inputs(self):
+        self.assert_illegal_argument(None)
+        self.assert_illegal_argument("12")
+        self.assert_illegal_argument("1234")
+
