862e215ed3714fbf426aeff6efab09c101d19e2d
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-13 14:05:40 +0900

    [feature] raise TypeError Exception when duplicated number

--- baseballGame/game.py
+++ baseballGame/game.py
@@ -6,6 +6,12 @@ class Game:
         if len(guess) != 3:
             raise TypeError()
 
+        if guess[0] == guess[1] or \
+            guess[0] == guess[2] or \
+            guess[1] == guess[2]:
+            raise TypeError()
+
+
         for number in guess:
             if ord('0') <= ord(number) <= ord('9'):
                 raise TypeError()
