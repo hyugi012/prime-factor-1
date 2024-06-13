db44ab3858cdb3ced44ec267de1f6aa572e90cf8
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-13 09:51:41 +0900

    [refactor] page 37

--- prime_factors.py
+++ prime_factors.py
@@ -6,8 +6,9 @@ class PrimeFactors:
         factors = []
         if number > 1:
             if number == 4:
-                factors.append(2)
-                factors.append(2)
+                while number % 2 == 0:
+                    factors.append(2)
+                    number //= 2
             else:
                 factors.append(number)
 
