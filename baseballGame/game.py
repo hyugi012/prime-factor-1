cc5e3da68741f36fc796000d6791b9ab183a7c32
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-13 14:07:03 +0900

    [refactor] raise TypeError Exception

--- baseballGame/game.py
+++ baseballGame/game.py
@@ -1,17 +1,19 @@
 class Game:
     def guess(self, guess):
+        self.asset_illegal_input(guess)
+
+    def asset_illegal_input(self, guess):
         if guess is None:
             raise TypeError()
-
         if len(guess) != 3:
             raise TypeError()
-
-        if guess[0] == guess[1] or \
-            guess[0] == guess[2] or \
-            guess[1] == guess[2]:
+        if self.is_duplicated_numbers(guess):
             raise TypeError()
-
-
         for number in guess:
             if ord('0') <= ord(number) <= ord('9'):
                 raise TypeError()
+
+    def is_duplicated_numbers(self, guess):
+        return guess[0] == guess[1] or \
+            guess[0] == guess[2] or \
+            guess[1] == guess[2]
