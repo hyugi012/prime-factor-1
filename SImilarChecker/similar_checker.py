8527e0da7a15c0d6301405f1fe5bc6d238d3adef
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-13 15:56:51 +0900

    [feature] assert invalid inputs

--- SImilarChecker/similar_checker.py
+++ SImilarChecker/similar_checker.py
@@ -1,2 +1,14 @@
 class SimilarChecker:
-    pass
+
+    def check_length(self, str1, str2):
+        self.assert_illegal_input(str1)
+        self.assert_illegal_input(str2)
+        pass
+
+    def assert_illegal_input(self, s):
+        if s is None:
+            raise TypeError
+
+        for char in s:
+            if not ord("A") <= ord(char) <= ord("Z"):
+                raise TypeError
--- SImilarChecker/test_similar_checker.py
+++ SImilarChecker/test_similar_checker.py
@@ -1,6 +1,22 @@
-from unittest import TestCase
+from unittest import TestCase, skip
+
+from SImilarChecker.similar_checker import SimilarChecker
 
 
 class TestSimilarChecker(TestCase):
-    def test_similar(self):
-        pass
+
+    def setUp(self):
+        super().setUp()
+        self.checker = SimilarChecker()
+
+    def assert_illegal_input(self, str1, str2):
+        try:
+            self.checker.check_length(str1, str2)
+            self.fail()
+        except TypeError:
+            pass
+
+    def test_assert_invalid_inputs(self):
+        self.assert_illegal_input(None, None)
+        self.assert_illegal_input("abc", "AbC")
+        self.assert_illegal_input("123", "12A")
