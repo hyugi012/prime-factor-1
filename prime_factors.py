395991c368df0e565018a3f3efb2ad53f43fb109
sanghyuk.lee <sogood.lee@samsung.com>
2024-06-13 09:50:21 +0900

    [feature] page 35

--- prime_factors.py
+++ prime_factors.py
@@ -5,6 +5,10 @@ class PrimeFactors:
     def of(self, number) -> list:
         factors = []
         if number > 1:
-            factors.append(number)
+            if number == 4:
+                factors.append(2)
+                factors.append(2)
+            else:
+                factors.append(number)
 
         return factors
